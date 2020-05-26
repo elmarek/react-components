9a8bc336b5747cbefcdfff505fa67dd042c012a7
elmarek <mareks@gmx.com>
2020-05-26 12:08:36 -0700

    First Commit

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
 // TODO
+console.log('hello world')
+
+var GroceryList = () => (
+  <div>
+    <h2>Grocery List:</h2>
+    <GroceryListItems items={['Watermelon', 'Crushed Lemons', 'Already sipped gatorade']}/>
+  </div>
+);
+
+var GroceryListItems = (props) => (
+  <ul>
+    <li>{props.items[0]}</li>
+    <li>{props.items[1]}</li>
+    <li>{props.items[2]}</li>
+ </ul>
+)
+
+ReactDOM.render(<GroceryList />, document.getElementById("app"));
