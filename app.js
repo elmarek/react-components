1336b1cddaa63cb12b17fea5c394c6dc9b00d395
elmarek <mareks@gmx.com>
2020-05-26 12:33:10 -0700

    Refactored Grocery List Item

--- app.js
+++ app.js
@@ -1,12 +1,29 @@
 // TODO
 console.log('hello world')
 
-var GroceryList = () => (
-  <div>
-    <h2>Grocery List:</h2>
-    <GroceryListItems items={['Watermelon', 'Crushed Lemons', 'Already sipped gatorade']}/>
-  </div>
-);
+class GroceryList extends React.Component {
+  // A `constructor` method is expected on all ES6 classes
+  // When React instantiates the component,
+  // it will pass `props` to the constructor
+  constructor(props) {
+    // Equivalent to ES5's React.Component.call(this, props)
+    super(props);
+  }
+  // Every class component must have a `render` method
+  // Stateless functional components are pretty much just this method
+
+  render() {
+
+    // `props` is no longer passed as an argument,
+    // but instead accessed with `this.props`
+    return (
+    <div>
+     <h2>Grocery List:</h2>
+      <GroceryListItems items={['Watermelon', 'Pre-Crushed Lemons', 'Once-Sipped Gatorade']}/>
+    </div>
+    );
+  }
+};
 
 var GroceryListItems = (props) => (
   <ul>
@@ -17,3 +34,4 @@ var GroceryListItems = (props) => (
 )
 
 ReactDOM.render(<GroceryList />, document.getElementById("app"));
+
